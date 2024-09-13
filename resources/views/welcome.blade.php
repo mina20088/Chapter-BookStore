9fc00ae504264ae16bbbf50e741dba177e6d19d5
mina remon <minakiroollos@gmail.com>
2024-09-13 10:09:56 +0300

    changed the books card body content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,10 @@
 
 @section('title','Home')
 @section('content')
+    <!--Hero Section-->
     @include('partials._hero-section')
+    <!--Author Section-->
     @include('partials._author-section')
-    <section class="highly-ranked-books-section bg-dark mt-sm-5 mb-md-10 px-md-0 py-lg-7 py-sm-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="display-3 text-center">Highly Rated Reads</h1>
-                    <x-slider/>
-                </div>
-            </div>
-        </div>
-    </section>
+    <!--Highly-Ranked-Section-->
+    @include('partials.highly-ranked-books-section')
 @endsection
