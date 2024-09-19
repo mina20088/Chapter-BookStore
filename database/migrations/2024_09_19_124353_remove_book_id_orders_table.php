56141544638a8476035c6e25a998424bacf2cc94
mina remon <minakiroollos@gmail.com>
2024-09-19 15:48:49 +0300

    Add 'OrderItems' truncation and migration file
    Included the truncation of 'OrderItems' table in DatabaseSeeder. Added a new migration file to modify the 'orders' table. Cleaned up unused code in the seeder to streamline database setup.

--- database/migrations/2024_09_19_124353_remove_book_id_orders_table.php
+++ database/migrations/2024_09_19_124353_remove_book_id_orders_table.php
@@ -11,7 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('book_id');
+        });
     }
 
     /**
@@ -19,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('book_id')->after('order_date');
+        });
     }
 };
