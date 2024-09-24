840814bd1a9b1526b0d7a1bc4799b02052fd475d
mina remon <minakiroollos@gmail.com>
2024-09-24 03:30:58 +0300

    Add Shipment model and factory
    This commit introduces the Shipment model with relevant fillable fields and a relationship to the Order model. Additionally, a corresponding factory is added to facilitate the creation of Shipment instances for testing and seeding purposes.

---
+++ app/Models/Shipment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Shipment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "order_id",
+        "estimated_delivery_date",
+        "delivery_date",
+        "status",
+        "cost",
+    ];
+
+    public function order():BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
---
+++ database/factories/ShipmentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\OrderStatus;
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Shipment>
+ */
+class ShipmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+             'order_id' => Order::factory(),
+             'estimated_delivery_date' => $this->faker->date(),
+             'delivery_date' => $this->faker->date(),
+             'status' => $this->faker->randomElement(OrderStatus::class),
+             'cost' => $this->faker->randomFloat(10),
+        ];
+    }
+}
