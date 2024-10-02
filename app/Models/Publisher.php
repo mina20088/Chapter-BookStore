16344da921cbcdf0267becf288a4b15a003416a9
mina remon <minakiroollos@gmail.com>
2024-10-02 17:56:23 +0300

    Add truncate method to Publisher model
    Introduced a static method to truncate the publishers table using DB facade. This addition provides an easier way to clear all records from the publishers table. Also, minor syntax correction in the books method signature.

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class Publisher extends Model
 {
@@ -23,8 +24,15 @@ class Publisher extends Model
         'comments'
     ];
 
-    public function books ():hasMany
+    public static function truncate(): null
+    {
+        return DB::table('publishers')->truncate();
+    }
+
+    public function books(): HasMany
     {
         return $this->hasMany(Book::class);
     }
+
+
 }
