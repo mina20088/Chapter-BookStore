2f51370d0e8886d4170e64cf24c7a6b1354bd041
mina remon <minakiroollos@gmail.com>
2024-10-04 16:42:44 +0300

    Update AuthorsSeeder to use predefined author data
    Replaced the random author generation with predefined author data leveraging the `AuthorsEnum` class. This ensures consistent and controlled seeding of the authors table for development and testing environments.

--- database/seeders/AuthorsSeeder.php
+++ database/seeders/AuthorsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\AuthorsEnum;
 use App\Models\Author;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,37 @@ class AuthorsSeeder extends Seeder
     {
         \Schema::disableForeignKeyConstraints();
         Author::truncate();
-        Author::factory()->count(3)->create();
+        Author::factory()->createMany([
+           [
+               'first_name' => "Olivia",
+               'middle_name' => null,
+               'last_name' => "Wilson",
+               'image' => AuthorsEnum::Alice
+           ],
+            [
+                'first_name' => "Aron",
+                'middle_name' => null,
+                'last_name' => "Loeb",
+                'image' => AuthorsEnum::Aron
+            ],
+            [
+                'first_name' => "Matt",
+                'middle_name' => null,
+                'last_name' => "Zhang",
+                'image' => AuthorsEnum::Matt
+            ],
+            [
+                'first_name' => "Chiaki",
+                'middle_name' => null,
+                'last_name' => "Zhang",
+                'image' => AuthorsEnum::Chiki
+            ],
+            [
+                'first_name' => "Lars",
+                'middle_name' => null,
+                'last_name' => "Peeters",
+                'image' => AuthorsEnum::Lars
+            ],
+        ]);
     }
 }
