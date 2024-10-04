317335e48d353a5dbe5c11dd9f9ee62be6888539
mina remon <minakiroollos@gmail.com>
2024-10-04 16:42:31 +0300

    Update image paths in AuthorsEnum
    Moved author image paths to the "authors" subdirectory to improve organization. This change ensures all author images are stored in a dedicated folder, making the file structure clearer and easier to manage.

--- app/Enums/AuthorsEnum.php
+++ app/Enums/AuthorsEnum.php
@@ -4,11 +4,11 @@ namespace App\Enums;
 
 enum AuthorsEnum :string
 {
-    case Alice  = "storage/images/Alice.png";
-    case Aron = "storage/images/Aron.png";
-    case  Chiki = "storage/images/Chiki.png";
-    case Lars = "storage/images/Lars.png";
-    case Matt = "storage/images/Matt.png";
-    case Olivia = "storage/images/Olivia.png";
+    case Alice  = "storage/images/authors/Alice.png";
+    case Aron = "storage/images/authors/Aron.png";
+    case  Chiki = "storage/images/authors/Chiki.png";
+    case Lars = "storage/images/authors/Lars.png";
+    case Matt = "storage/images/authors/Matt.png";
+    case Olivia = "storage/images/authors/Olivia.png";
 
  }
