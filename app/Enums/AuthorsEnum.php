a4046d069a7599e157e7db2958143dd0944c16e5
mina remon <minakiroollos@gmail.com>
2024-10-04 16:23:30 +0300

    Add AuthorsEnum to manage author image paths
    Introduced a new enumeration, AuthorsEnum, to handle the storage paths of various author images. This change aids in maintaining consistency and ease of access to author image resources by providing a centralized enum for reference.

---
+++ app/Enums/AuthorsEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+enum AuthorsEnum :string
+{
+    case Alice  = "storage/images/Alice.png";
+    case Aron = "storage/images/Aron.png";
+    case  Chiki = "storage/images/Chiki.png";
+    case Lars = "storage/images/Lars.png";
+    case Matt = "storage/images/Matt.png";
+    case Olivia = "storage/images/Olivia.png";
+
+ }
