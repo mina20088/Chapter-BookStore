a09b4df5aaf2c5afe7c88ae9556a5df179fdffd6
mina remon <minakiroollos@gmail.com>
2024-10-06 17:02:24 +0300

    Rename welcome.blade.php to Home/index.blade.php
    Updated view path to improve file organization and structure. Added parameters to hero and highly-ranked books sections to enhance dynamic content rendering.

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,11 +1,12 @@
 @extends("layouts.guest")
 
 @section('title','Home')
+
 @section('content')
     <!--Hero Section-->
-    @include('partials._hero-section')
+    @include('partials._hero-section',['new' => $new])
     <!--Author Section-->
     @include('partials._author-section')
     <!--Highly-Ranked-Section-->
-    @include('partials.highly-ranked-books-section')
+    @include('partials.highly-ranked-books-section',['ranked' => $rank])
 @endsection
