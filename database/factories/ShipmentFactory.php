c6dcf33538c5761438827300fc8a5ad3161b6d02
mina remon <minakiroollos@gmail.com>
2024-10-06 17:03:58 +0300

    Simplify ShipmentFactory documentation
    Corrected the docblock annotation in ShipmentFactory. Replaced the fully qualified namespace with a simplified version and updated the extend statement format for clarity.

--- database/factories/ShipmentFactory.php
+++ database/factories/ShipmentFactory.php
@@ -4,11 +4,12 @@ namespace Database\Factories;
 
 use App\Enums\OrderStatus;
 use App\Models\Order;
+use App\Models\Shipment;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Shipment>
+ * @extends Factory<Shipment>
  */
 class ShipmentFactory extends Factory
 {
