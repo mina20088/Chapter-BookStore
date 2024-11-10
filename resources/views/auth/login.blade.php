3c15a31eddd56c72fdd166cc4b7ecb99ed575bdc
mina remon <minakiroollos@gmail.com>
2024-11-10 03:54:18 +0200

    changed input styles in bootstrap file ,created the authentication form in auth.blade.php,add the logo section to the login.blade.php,added new photo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,4 +2,45 @@
     <x-slot name="title">
         Login
     </x-slot>
+        <div class="col-6">
+            <h1>Ready to start your</h1>
+            <h1>reading journey</h1>
+            <p>Signup to our website now and start reading</p>
+            <from action="/login" method="Post">
+                @csrf
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control border-top-0 border-end-0 border-start-0" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control border-top-0 border-end-0 border-start-0" id="floatingInput" placeholder="password">
+                    <label for="floatingInput">password</label>
+                </div>
+                <div class="d-flex justify-content-between">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <label class="form-check-label" for="flexCheckDefault">
+                            remember me
+                        </label>
+                    </div>
+                    <div>
+                        <a href="">Forget Password</a>
+                    </div>
+                </div>
+
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-dark mt-sm-5">
+                        Login
+                    </button>
+                </div>
+                <div class="d-flex justify-content-center mt-4">
+                    <p>If you don`t have an account <a href="">Register</a></p>
+                </div>
+            </from>
+        </div>
+        <div class="col-6">
+            <img class="img-fluid" src="{{ Vite::asset("resources/images/Login_form.png") }}" alt="login_form_illustration">
+        </div>
+
+
 </x-auth-layout>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,8 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? "laravel" }}</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
+
+<div class="logo-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex flex-row justify-content-between align-items-center">
+                    <img src="{{ Vite::asset('resources/images/logo-colored.png') }}" class="img-fluid" width="150">
+                    <a class="btn btn-primary h-25" href="{{ route('home.index') }}">Back</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content-section">
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height: 87vh">
+            {{ $slot }}
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
