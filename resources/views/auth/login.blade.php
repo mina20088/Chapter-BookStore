720e7c21fadf67c8779d276b070574c3e3cb3a30
mina remon <minakiroollos@gmail.com>
2024-11-12 19:15:59 +0200

    removed hl from forgot-password.blade.php,removed the login html from the login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,44 +2,13 @@
     <x-slot name="title">
         Login
     </x-slot>
-        <div class="col-6">
+        <div class="col-sm-12 mt-sm-3 order-sm-1 order-md-1 text-sm-center col-md-6">
             <h1>Welcome Back! Ready to continue your</h1>
             <h1>reading journey</h1>
             <p>Signing to our website now and start reading</p>
             <livewire:partials.login-form/>
-{{--            <from action="/login" method="Post">
-                @csrf
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control border-top-0 border-end-0 border-start-0" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control border-top-0 border-end-0 border-start-0" id="floatingInput" placeholder="password">
-                    <label for="floatingInput">password</label>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
-                            remember me
-                        </label>
-                    </div>
-                    <div>
-                        <a href="">Forget Password</a>
-                    </div>
-                </div>
-
-                <div class="d-grid">
-                    <button type="submit" class="btn btn-dark mt-sm-5">
-                        Login
-                    </button>
-                </div>
-                <div class="d-flex justify-content-center mt-4">
-                    <p>If you don`t have an account <a href="">Register</a></p>
-                </div>
-            </from>--}}
         </div>
-        <div class="col-6">
+        <div class="col-sm-12 mt-sm-3 col-md-6">
             <img class="img-fluid" src="{{ Vite::asset("resources/images/Login_form.png") }}" alt="login_form_illustration">
         </div>
 </x-auth-layout>
